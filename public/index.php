d41fbd0b4b248ea8e3907f66d546a3550b906279
Alexandre Ferreira <shieldforce2@gmail.com>
2022-09-23 13:19:33 -0300

    Começando com o desenvolviment do frame-sf

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Shieldforce\FrameSf\BootSystem;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require "../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable("../"); $dotenv->load();
+
+BootSystem::start();
---
+++ src/BootSystem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Shieldforce\FrameSf;
+
+class BootSystem
+{
+    public static function start()
+    {
+        echo "iniciando a aplicação!";
+    }
+}
